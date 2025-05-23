de047b8bf3d4950f4e5f189156ad08618394760b
ismael.wane <ismael.wane@universite-paris-saclay.fr>
2025-05-23 09:25:57 +0200

    version 1.0.0

--- exploration_jupytercards/tools.py
+++ exploration_jupytercards/tools.py
@@ -1,7 +1,7 @@
 import os
 from exploration_jupytercards import toDict
 from exploration_jupytercards import file
-
+import sys
 
 def toDict_tools() :
     l = []
@@ -16,13 +16,20 @@ def toDict_tools() :
     file.writeInFile(l, chemin_courant+"build_/def_converted.json")
 
 def extract_tools() :
-    l = []
-    chemin_courant = os.getcwd()
-    dossier = os.listdir("build_/JSON")
-    for fichier in dossier:
-        if fichier.endswith(".json"):
-            dict_ = toDict.convToDict("build_/JSON/"+fichier)
-            l.append(dict_)
-            print(f"--- {fichier} traité ---\n")
+    #  Argument positionnel : chemin du répertoire « content »
+    current_dir = os.getcwd()
+    content_dir = sys.argv[1]
+    #content_par = os.path.dirname(content_dir)
+    if not os.path.isdir(content_dir):
+        raise FileNotFoundError(f"Le répertoire '{content_dir}' est introuvable.")
     
-    file.writeInFile(l, chemin_courant+"build_/def_converted.json")
+    os.makedirs(current_dir+"/JSON", exist_ok=True)
+    
+    file_counter = 0
+    for fichier in os.listdir(content_dir):
+        if fichier.endswith(".json"):
+            chemin = os.path.join(content_dir, fichier)
+            extract.extract_admonition_ast_from_file(chemin, file_counter)
+            file_counter+=1
+            print(file_counter)
+            print(f"— {fichier} traité —\n")
