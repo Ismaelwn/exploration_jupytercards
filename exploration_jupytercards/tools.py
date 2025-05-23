e6374a3f6df3dc78648f3834d6427d65fc5ae18c
ismael.wane <ismael.wane@universite-paris-saclay.fr>
2025-05-23 08:05:25 +0200

    version 1.0.0

---
+++ exploration_jupytercards/tools.py
@@ -0,0 +1,28 @@
+import os
+from exploration_jupytercards import toDict
+from exploration_jupytercards import file
+
+
+def extract_tools() :
+    l = []
+    chemin_courant = os.getcwd()
+    dossier = os.listdir("build_/JSON")
+    for fichier in dossier:
+        if fichier.endswith(".json"):
+            dict_ = toDict.convToDict("build_/JSON/"+fichier)
+            l.append(dict_)
+            print(f"--- {fichier} traité ---\n")
+    
+    file.writeInFile(l, chemin_courant+"build_/def_converted.json")
+
+def toDict_tools() :
+    l = []
+    chemin_courant = os.getcwd()
+    dossier = os.listdir("build_/JSON")
+    for fichier in dossier:
+        if fichier.endswith(".json"):
+            dict_ = toDict.convToDict("build_/JSON/"+fichier)
+            l.append(dict_)
+            print(f"--- {fichier} traité ---\n")
+    
+    file.writeInFile(l, chemin_courant+"build_/def_converted.json")
